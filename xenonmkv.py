51d825a693db5a511344584c7dd1228cf9fc53ef
Jake Billo <jake@jakebillo.com>
2012-09-01 14:50:44 -0400

    Initial structure

--- xenonmkv.py
+++ xenonmkv.py
@@ -1,16 +1,32 @@
-# XenonMKV for PlayBook
+#!/usr/bin/python
+
+# XenonMKV for PlayBook/revision 2.0
 
 import argparse
 import sys
+import os
 
-def usage():
-	print """
-Usage: %s filename [options]
+parser = argparse.ArgumentParser(description='Parse command line arguments for XenonMKV.')
+parser.add_argument('source_file', help='Path to the source MKV file')
+parser.add_argument('-d', '--destination', help='Directory to output the destination .mp4 file (default: current directory)', default='.')
 
-"""
+if len(sys.argv) < 2:
+	parser.print_help()
 	sys.exit(1)
 
-if sys.argv < 2:
-	usage()
+# Always ensure destination path ends with a slash
+if not destination.endswith('/'):
+	destination += '/'
+
+# Check if source file exists
+if os.path.isfile(source_file):
+	print "Error: File " + source_file + " does not exist"
+	sys.exit(1)
+
+# Check if destination directory exists
+if not os.path.isdir(destination):
+	print "Error: Destination " + destination + " does not exist"
+	sys.exit(1)
+
 
 
