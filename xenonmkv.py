d7124aaa93bee31c8fd95699ffe8bdcb359ac3b4
Jake Billo <jake@jakebillo.com>
2012-09-02 11:11:00 -0400

    Updates prior to adding multiple track support in mediainfo

--- xenonmkv.py
+++ xenonmkv.py
@@ -8,21 +8,28 @@ import os
 import subprocess
 import logging
 
+import reference_frame
+
 class MKVInfoParser:
 	@staticmethod
-	def parse_track_number(output):
-		detect = "|  + Track number: "
+	def _parse_to_newline(output, detect, what="value"):
 		if detect in output:
 			output = output[output.index(detect) + len(detect):]
 			if "\n" in output:
 				output = output[0:output.index("\n")]
-
-			log.debug("Detected track number %s from mkvinfo output" % output)
-			return int(output)
+			log.debug("Detected %s '%s' from mkvinfo output" % (what, output))
+			return output
 		else:
-			raise Exception("Could not parse track number from mkvinfo")
+			raise Exception("Could not parse %s from mkvinfo" % what)
+		
+	@staticmethod
+	def parse_track_number(output):
+		return int(MKVInfoParser._parse_to_newline(output, "|  + Track number: ", "track number"))
 
-	
+	@staticmethod
+	def parse_track_type(output):
+		return MKVInfoParser._parse_to_newline(output, "|  + Track type: ", "track type")
+
 
 class MKVFile():
 	path = ""
@@ -35,6 +42,7 @@ class MKVFile():
 	def get_path(self):
 		return self.path
 
+	# Open the mkvinfo process and parse its output.
 	def get_mkvinfo(self):
 		track_count = 0
 		log.debug("Executing 'mkvinfo %s'" % self.get_path())
@@ -42,9 +50,31 @@ class MKVFile():
 		log.debug("mkvinfo finished; attempting to parse output")
 		self.parse_mkvinfo(result)
 
+	# Open the mediainfo process to obtain detailed info on the file.
+	def get_mediainfo_video(self):
+		parameters = "Video;%Height%,%Width%,%Format_Settings_RefFrames%,%Language%,%FrameRate%,%CodecID%"
+		log.debug("Executing 'mediainfo %s %s'" % (parameters, self.get_path()))
+		result = subprocess.check_output(["mediainfo", "--Inform=" + parameters, self.get_path()])
+		log.debug("mediainfo finished; attempting to parse output for video settings")
+		return self.parse_mediainfo(result)
+
+	def get_mediainfo_audio(self):
+		parameters = "Video;%Height%,%Width%,%Format_Settings_RefFrames%"
+		log.debug("Executing 'mediainfo %s %s'" % (parameters, self.get_path()))
+		result = subprocess.check_output(["mediainfo", "--Inform=" + parameters, self.get_path()])
+		log.debug("mediainfo finished; attempting to parse output for audio settings")
+		return self.parse_mediainfo(result)
+
+	def parse_mediainfo(self, result):
+		output = result.split(",")
+
+		return output
+
+	# Return the fixed duration of the MKV file.
 	def get_duration(self):
 		return self.duration
 
+	# Parse the 'duration' line in the mkvinfo output to estimate a duration for the file.
 	def parse_audio_duration(self, output):
 		audio_int = 0
 
@@ -68,8 +98,7 @@ class MKVFile():
 
 		return audio_int
 
-
-
+	# Parse the output from mkvinfo for the file.
 	def parse_mkvinfo(self, result):
 		track_detect_string = "| + A track"
 		
@@ -86,28 +115,48 @@ class MKVFile():
 			else:
 				break
 
-			# Get track type and number out of this block		
-			track.set_number(MKVInfoParser.parse_track_number(track_info))
+			# Get track type and number out of this block
+			track_type = MKVInfoParser.parse_track_type(track_info)
+			track_number = MKVInfoParser.parse_track_number(track_info)
+
+			if track_type == "video":
+				track = VideoTrack()
+				track.number = track_number
+
+				# height, width, reference_frames are returned in mediainfo
+				mediainfo = self.get_mediainfo_video()
+				track.height = int(mediainfo[0])
+				track.width = int(mediainfo[1])
+				track.reference_frames = int(mediainfo[2])
+				track.language = mediainfo[3]
+				track.frame_rate = float(mediainfo[4])
+				track.codec_id = mediainfo[5]
+
+				log.debug("Video track has dimensions %ix%i with %i reference frames" % (track.width, track.height, track.reference_frames))
+				log.debug("Video track has %f FPS and codec %s" % (track.frame_rate, track.codec_id))
+
+				if self.reference_frames_exceeded(track):
+					raise Exception("Video file contains too many reference frames to play properly on low-power devices")
+				
+				
+					
+
+			elif track_type == "audio":
+				track = AudioTrack()
+				track.number = track_number
+
+				mediainfo = self.get_mediainfo_audio()
 			
+			else:
+				raise Exception("Unrecognized track type %s" % track_type)
 
 
-		# Split output into new line array
-		#lines = result.split("\n")
-
-"""
-		for line in range(0, len(lines)):
-			# We need a numerical index so we can skip forward here
-			line = lines[line]
-			if line == track_detect_string:
-				# Create new track object
-				track_count += 1
-				track = MKVTrack()
-"""
-
+	def reference_frames_exceeded(self, video_track):
+		return reference_frame.ReferenceFrameValidator.validate(video_track.height, video_track.width, video_track.reference_frames)
 
 class MKVTrack():
 	number = 0
-	uid = track_type = ""
+	uid = track_type = language = codec_id = ""
 
 	def set_number(self, number):
 		self.number = number
@@ -126,6 +175,13 @@ class MKVTrack():
 
 	def get_type(self):
 		return self.track_type
+
+class VideoTrack(MKVTrack):
+	height = width = reference_frames = 0
+	frame_rate = 0.0
+
+class AudioTrack(MKVTrack):
+	length = 0
 
 parser = argparse.ArgumentParser(description='Parse command line arguments for XenonMKV.')
 parser.add_argument('source_file', help='Path to the source MKV file')
