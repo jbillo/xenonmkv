f33ec5b72413da948910ae3fb5f787070473b5a2
jbillo <jake@jakebillo.com>
2012-10-08 23:53:09 -0400

    Details on faac blocking on Windows

--- encoder.py
+++ encoder.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import sys
 
 from process_handler import ProcessHandler
@@ -25,9 +24,21 @@ class FAACEncoder():
 			return True
 
 		cmd = [self.args.tool_paths["faac"], "-q", str(self.args.faac_quality), self.file_path]
+		self.log.debug("Executing '%s'" % ' '.join(cmd))
 		ph = ProcessHandler(self.args)
 		process = ph.start_process(cmd)
 
+		"""FIXME: On Windows, this causes issues. Technically if Linux/OSX didn't have any content
+		in stderr, faac could run into the same error.
+		
+		The problem comes down to the fact that stderr.read(1) is blocking. If there is no content
+		output to stderr, the call will never return and proceed to the next phase.
+		
+		An answer to http://stackoverflow.com/questions/375427/non-blocking-read-on-a-subprocess-pipe-in-python
+		seems to have the method - combining a Queue and threading in order to allow non-blocking reads.
+		
+		This should probably all be integrated into the ProcessHandler class.
+		"""
 		while True:
 			err = process.stderr.read(1)
 			if err == '' and process.poll() != None:
